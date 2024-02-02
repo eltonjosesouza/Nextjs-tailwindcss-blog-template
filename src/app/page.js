9447c9af9bcd53454cc4107783be45ad165f9e55
Elton José <elton.jose.souza@gmail.com>
2024-02-02 16:32:42 -0300

    add speed insights

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,8 @@ import HomeCoverSection from "../components/Home/HomeCoverSection";
 import FeaturedPosts from "../components/Home/FeaturedPosts";
 import RecentPosts from "../components/Home/RecentPosts";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 
 export default function Home() {
 
@@ -12,6 +14,7 @@ export default function Home() {
       <FeaturedPosts blogs={allBlogs} />
       <RecentPosts blogs={allBlogs} />
       <Analytics />
+      <SpeedInsights />
     </main>
   )
 }
